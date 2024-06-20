6160a0c88e3986c9d310a91177fa482a5143c3c6
gaya8 <gaya8@KTH>
2024-06-20 21:34:20 +0900

    배열 추가 연습

---
+++ First_Project/src/day4/Day4_Test3.java
@@ -0,0 +1,17 @@
+package day4;
+
+import java.util.Arrays;
+
+public class Day4_Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {5, 3, 4, 1, 2};
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+		System.out.println(Math.max(4, 9));
+	}
+
+}
