7faea772b465c55f2353689a6625b4c67352308e
gaya8 <gaya8@KTH>
2024-06-24 20:19:15 +0900

    클래스 연습문제

---
+++ First_Project/src/day6/MultipleCalcClass.java
@@ -0,0 +1,11 @@
+package day6;
+
+public class MultipleCalcClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MultipleCalc.multiple(3, 5));
+		System.out.println(MultipleCalc.multiple(2, 3, 4));
+	}
+
+}
---
+++ First_Project/src/day6/PersonClass.java
@@ -0,0 +1,15 @@
+package day6;
+
+import java.util.Arrays;
+
+public class PersonClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person("123-123");
+		System.out.println(person.getName());
+		System.out.println(person.getAge());
+		System.out.println(person.getPhone());
+	}
+
+}
